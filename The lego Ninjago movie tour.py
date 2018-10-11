625d61f7782544eb8b15c425b2eac9e9ec708a4c
mroufail <44034898+mroufail@users.noreply.github.com>
2018-10-11 21:46:12 +0100

    number 9 assignment on chapter 1 of python

---
+++ The lego Ninjago movie tour.py
@@ -0,0 +1,16 @@
+#This program  will ask what type of ninja you would like to be
+# This program was written on 11 october 2018
+
+def main():
+    print("Have you wached the lego Ninjago movie what is your best Ninja suit?")
+    bestNinjaSuit = input()
+    print("{0} that is a nice suit ".format(bestNinjaSuit))
+    print("Do you like The legoNinjagomovie or LegoNinjagomastersspinjitsu")
+    LegoNinjagoChoices = input()
+    print("{0} good choice ".format(LegoNinjagoChoices))
+    return;
+
+main()
+    
+    
+    
---
+++ numbers proggramming.py
@@ -0,0 +1,21 @@
+#the program will ask chose your number then add them
+
+def main():
+    print("choose a number")
+    number1= int(input())
+    print("choose another number")
+    number2=int(input())
+    total=number1+number2
+    print("the sum of {0} and {1} is {2}".format(number1,number2,total))
+    multiple=number1*number2
+    print("Multiplying {0} and {1} is {2}".format(number1,number2,multiple))
+
+
+
+
+    
+    return;
+
+main()
+          
+    
